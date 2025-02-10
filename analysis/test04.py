6a80adf3093d2bf5179aca2f9f6c519e18025336
bigOrange123456789 <2814255951@qq.com>
2025-02-10 16:34:00 +0800

    更新了绘图的代码

--- analysis/test04.py
+++ analysis/test04.py
@@ -2,8 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
-indicatorFlag='f1'
-assert indicatorFlag in ['total_loss', 'f1', 'AUC', 'pr', 'recall', 'Acc', 'Sp', 'JC']
+# 创建2行4列的子图布局
+fig, axs = plt.subplots(2, 4, figsize=(12, 6))  # figsize调整整体大小
+indicatorFlagList = ['total_loss', 'f1', 'AUC', 'pr', 'recall', 'Acc', 'Sp', 'JC']
+axsList = [ axs[0,0],axs[0,1],axs[0,2],axs[0,3], axs[1,0],axs[1,1],axs[1,2],axs[1,3] ]
+
 # pr : precision
 # sp:  specificity
 #   特异性是衡量模型在负样本（Negative Class）上表现的指标，它反映了模型正确识别负样本的能力。
@@ -26,7 +29,10 @@ def extract_float_from_tensor_string(tensor_string):
         return float(match.group(1))
     else:
         raise ValueError(f"无法从字符串中提取数值: {tensor_string}")
-def progressFile(name,color):
+def progressFile(id,name,color):
+    indicatorFlag=indicatorFlagList[id]
+    axs0=axsList[id]
+    # assert indicatorFlag in ['total_loss', 'f1', 'AUC', 'pr', 'recall', 'Acc', 'Sp', 'JC']
     path="../logs/"+name
     # 读取CSV文件
     df = pd.read_csv(path+'/val_train_f1.csv')
@@ -42,18 +48,18 @@ def progressFile(name,color):
     # 绘制折线图
     # plt.figure(figsize=(10, 6))  # 设置图形大小
     # plt.plot(df['epoch'], df['f1'], marker='o', linestyle='-', color='b')  # 绘制折线图
-    plt.plot(df['epoch'], df[indicatorFlag], linestyle='-', color=color,label=name)  # 绘制折线图
+    axs0.plot(df['epoch'], df[indicatorFlag], linestyle='-', color=color,label=name)  # 绘制折线图
 
     # # 隐藏纵坐标轴的刻度
     # plt.yticks([])
 
     # 绘制竖直直线
     max_indicator_epoch1 = df.loc[df[indicatorFlag].idxmax(), 'epoch']
-    plt.axvline(x=max_indicator_epoch1, color=color, linestyle='--', alpha=0.6)
+    axs0.axvline(x=max_indicator_epoch1, color=color, linestyle='--', alpha=0.6)
 
     # 绘制水平直线
     max_indicator_value1 = df[indicatorFlag].max()
-    plt.axhline(y=max_indicator_value1, color=color, linestyle='--', alpha=0.6)
+    axs0.axhline(y=max_indicator_value1, color=color, linestyle='--', alpha=0.6)
 
     # 计算最优结果之后的分数的均值和标准差
     max_indicator_epoch = df.loc[df[indicatorFlag].idxmax(), 'epoch']
@@ -62,34 +68,45 @@ def progressFile(name,color):
     std_indicator = remaining_epochs[indicatorFlag].std()
 
     # 在图中添加均值和标准差的注释
-    plt.text(max_indicator_epoch + 1, mean_indicator, 
+    axs0.text(max_indicator_epoch + 1, mean_indicator, 
             f'Max: {max_indicator_value1:.4f}\nMean: {mean_indicator:.4f}\nStd: {std_indicator:.4f}', 
             verticalalignment='center', horizontalalignment='left', 
             color=color, fontsize=10, bbox=dict(facecolor='white', alpha=1))
+    
+    axs0.legend()
 
-if True:
-    progressFile("FreeCOS-GuangYuan01",'r')
-    # progressFile("FreeCOS-GuangYuan02",'g')
-    # progressFile("FreeCOS-GuangYuan03",'b')
-    # progressFile("FreeCOS-GuangYuan04",'c')
-    # progressFile("FreeCOS-GuangYuan05",'m')
-    # progressFile("FreeCOS-GuangYuan06",'y') # 错误
-    # progressFile("FreeCOS-GuangYuan07",'k')
-    # progressFile("FreeCOS-GuangYuan09",'g')
-    # progressFile("FreeCOS-GuangYuan10",'b')
-    # progressFile("FreeCOS-GuangYuan11",'g')
-    # progressFile("FreeCOS-GuangYuan01",'r')
-    progressFile("FreeCOS-GuangYuan14",'g')
-    # progressFile("FreeCOS-GuangYuan17",'b')
+
+
+
+# if True:
+for id in range(len(axsList)):
+    indicatorFlag=indicatorFlagList[id]
+    axs0=axsList[id]
+    # progressFile(id,"FreeCOS-GuangYuan01",'r')
+    # progressFile(id,"FreeCOS-GuangYuan02",'g')
+    # progressFile(id,"FreeCOS-GuangYuan03",'b')
+    # progressFile(id,"FreeCOS-GuangYuan04",'c')
+    # progressFile(id,"FreeCOS-GuangYuan05",'m')
+    # progressFile(id,"FreeCOS-GuangYuan06",'y') # 错误
+    # progressFile(id,"FreeCOS-GuangYuan07",'k')
+    # progressFile(id,"FreeCOS-GuangYuan09",'g')
+    # progressFile(id,"FreeCOS-GuangYuan10",'b')
+    # progressFile(id,"FreeCOS-GuangYuan11",'g')
+    progressFile(id,"FreeCOS-GuangYuan01",'r')
+    progressFile(id,"FreeCOS-GuangYuan02",'g')
+    progressFile(id,"FreeCOS-GuangYuan16",'b')
     # progressFile("FreeCOS-GuangYuan17",'c')
-    plt.title('y:'+indicatorFlag+' x:epoch')  # 设置标题
+    axs0.set_title('y:'+indicatorFlag+' x:epoch')
+    '''
     plt.xlabel('Epoch')  # 设置横坐标标签
     plt.ylabel(indicatorFlag+' Score')  # 设置纵坐标标签
     # plt.legend(fontsize=10, title='Curve Names')  # 添加图例，并设置字体大小和标题
     plt.legend(fontsize=10)  # 添加图例，并设置字体大小和标题
     # plt.grid(True)  # 添加网格线
     # plt.xticks(df['epoch'])  # 设置横坐标刻度
-    plt.tight_layout()  # 自动调整布局
-    plt.show()  # 显示图形
+    '''
+plt.title('y:indicator x:epoch')  # 设置标题    
+plt.tight_layout()  # 自动调整布局
+plt.show()  # 显示图形
 
     
